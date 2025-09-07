eddf026d36c2bc53adc297d9c34c748965ecb428
SushyDev <xsushy@hotmail.com>
2025-09-07 13:46:27 +0200

    cleanup

--- lib/config/wallpaper-commands.nix
+++
@@ -1,8 +0,0 @@
-{ lib, customLib, config }:
-let
-	nix-plist-manager = config.nix-plist-manager;
-	users = nix-plist-manager.users;
-
-	wallpaperPath = "/System/Library/Desktop Pictures/Solid Colors/Black.png";
-	wallpaperCommands = lib.map (user: customLib.runAsUser user ("osascript -e 'tell application \"Finder\" to set the desktop picture to POSIX file \"${wallpaperPath}\"'")) users;
-in wallpaperCommands
